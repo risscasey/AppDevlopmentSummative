70f0f1b49b56922b7b116390d3e825fe67a335c1
Annie Isa'ako <270020786@bun2-204-19.local>
2019-09-17 13:26:38 +1200

    Need to get users working

---
+++ models/users.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  username: String,
+  password: String,
+  fullName: String,
+  email: String
+});
+
+module.exports = mongoose.model('User', usersSchema);
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const cors = require('cors');
 
 const config = require('./config.json');
 
+const User = require('./models/users')
+
 mongoose.connect(`mongodb+srv://${config.mongoDBUser}:${config.mongoDBPassword}@${config.mongoClusterName}.mongodb.net/formative?retryWrites=true&w=majority`, {useNewUrlParser: true});
 
 const db = mongoose.connection;
@@ -32,6 +34,26 @@ app.get('/', function(req, res){
     res.send('Welcome Digimart, a consumer to consumer platform where you can view, buy and sell items');
 });
 
+app.post('/users', function(req, res) {
+  User.findOne({ username: req.body.username }, function (err, checkUser) {
+        if(checkUser){
+            res.send('user already exists');
+        } else {
+            const hash = bcrypt.hashSync(req.body.password);
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                fullName: req.body.fullName,
+                username: req.body.username,
+                password: hash,
+                email: req.body.email
+            });
+            // Save user in database
+            user.save().then(result => {
+                res.send(result);
+            }).catch(err => res.send(err));
+        }
+    });
+});
 
 app.listen(port, () => {
     console.clear();
