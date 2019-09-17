41d06fc914b64b5259c1affc579def4bde7827be
Annie Isa'ako <270020786@bun2-204-19.local>
2019-09-17 13:46:09 +1200

    Working on registration

--- models/users.js
+++ models/users.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const usersSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  username: String,
-  password: String,
-  fullName: String,
-  email: String
+    _id: mongoose.Schema.Types.ObjectId,
+    username: String,
+    email: String,
+    password: String
 });
 
 module.exports = mongoose.model('User', usersSchema);
