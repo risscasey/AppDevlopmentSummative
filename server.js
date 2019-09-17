b8e5b4f817afec49e5158f582306c744711a9010
Annie Isa'ako <270020786@bun2-204-19.local>
2019-09-17 16:26:01 +1200

    small changes, have to get login working

--- server.js
+++ server.js
@@ -51,20 +51,13 @@ app.post('/listing', function(req, res){
 
 });
 
-<<<<<<< HEAD
+
 app.get('/allListings', function(req, res) {
   Listing.find().then(result => {
     res.send(result);
   });
 });
-=======
-app.get('/allListings', function(req, res){
-  Listing.find().then(result => {
-    console.log(result);
-    res.send(result);
-  })
-})
->>>>>>> master
+
 // larissa codes untill here
 
 app.post('/users', function(req, res) {
