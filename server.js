b10273b923cd075843e682111b499c7c7a2b861a
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-09-26 11:13:33 +1200

    Uploads folder

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(function(req, res, next){
   next();
 });
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.send('Welcome Digimart, a consumer to consumer platform where you can view, buy and sell items');
 });
 
